c900e98d468d8ded743a0f8717e1fd9d7b283d4d
David Cook <divergentdave@gmail.com>
2023-08-19 14:57:12 -0500

    Check in hacked up CI script

--- etc/ci/test-fiat-rust-crate-crypto-Ed448-Goldilocks.sh
+++ etc/ci/test-fiat-rust-crate-crypto-Ed448-Goldilocks.sh
@@ -5,10 +5,13 @@ set -ex
 ################################################################################
 # Tests for crate-crypto/Ed448-Goldilocks
 ################################################################################
-git clone https://github.com/crate-crypto/Ed448-Goldilocks.git --branch=master Ed448-Goldilocks || exit $?
+git clone https://github.com/divergentdave/Ed448-Goldilocks.git --branch=fiat-crypto-0.2 Ed448-Goldilocks || exit $?
 
 pushd Ed448-Goldilocks >/dev/null || exit $?
+git checkout 31a5230b8635fff6f4d118a1d36ac85d531d3c56 || exit $?
 
+mv Cargo.toml Cargo.toml.old
+head -n -3 Cargo.toml.old > Cargo.toml
 cat >> Cargo.toml <<EOF
 
 [patch.crates-io]
